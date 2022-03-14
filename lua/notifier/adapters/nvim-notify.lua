c628dd42ed0e3b0eabd7056c9fc7d5eda0a34388
Camille Dejoye <camilledejoye@gmail.com>
2022-03-14 01:39:09 +0100

    feat(nvim-notify): improve nvim-notify adapter to handle progress
    When I first started with this POC nvim-notify was not yet able to refresh an existing notification.
    Now that it's the case I updated the adapter to act accordingly and even have a little fun with an ASCII progress bar.

--- lua/notifier/adapters/nvim-notify.lua
+++ lua/notifier/adapters/nvim-notify.lua
@@ -1,17 +1,65 @@
-local notify = {}
+local notify = require('notify').notify
+local extension = {}
+local unicode_blocks = { '▏', '▎', '▍', '▌', '▋', '▊', '▉', '█' }
 
-function notify.notify(msg, log_level, opts)
+local function index_with_value(t)
+  local i = {}
+  for k = 1, #t do
+    table.insert(i, k)
+  end
+
+  return function()
+    local k = table.remove(i)
+    if k ~= nil then
+      return k, t[k]
+    end
+  end
+end
+
+local function progress_bar(percentage)
+  local bar = ''
+  -- multiply by 2 to make the bar twice as big
+  local percentage_done = percentage * 2
+  local percentage_remaining = 200 - percentage_done
+
+  for value, char in index_with_value(unicode_blocks) do
+    local number = math.floor(percentage_done / value)
+    percentage_done = math.fmod(percentage_done, value)
+    for _ = 1, number do
+      bar = bar .. char
+    end
+  end
+
+  for _ = 1, math.floor(percentage_remaining / #unicode_blocks) do
+    bar = bar .. ' '
+  end
+
+  return string.format('[ %s ] %.2f %%', bar, percentage)
+end
+
+function extension.notify(msg, log_level, opts)
   log_level = log_level or 2
   opts = opts or {}
 
-  require('notify').notify(msg, log_level, {
+  local notify_opts = {
     title = opts.summary or opts.app_name or nil,
-    -- I don't even try to handle the progress option since it's not supported yet
-    -- @see https://github.com/rcarriga/nvim-notify/issues/43
-  })
+  }
 
-  -- nvim-notify does not identify each notifications
-  return 0
+  if 0 ~= opts.replaces_id then
+    notify_opts.replace = opts.replaces_id
+  end
+
+  if 100 == opts.progress then
+    notify_opts.icon = ''
+    notify_opts.timeout = 2000 -- Must be reset otherwise keep original value
+    notify_opts.hide_from_history = false
+  elseif nil ~= opts.progress then
+    notify_opts.timeout = false
+    notify_opts.hide_from_history = true
+    msg = msg .. '\n' .. progress_bar(opts.progress)
+  end
+
+  return notify(msg, log_level, notify_opts).id
 end
 
-return notify
+return extension
